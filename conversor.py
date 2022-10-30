fc6df140d8047d33af7c542d5ceb6ced18f04092
Ilan Néstor Angeles Rodriguez <ilanangelesrodriguez@gmail.com>
2022-10-30 15:23:25 -0500

    Conversor de dolares

---
+++ conversor.py
@@ -0,0 +1,9 @@
+print("Bienvenido")
+print("Conversor de dolares")
+soles=input("Cantidad: S/.")
+soles=float(soles)
+valor_dolar=3.8
+dolar=soles/valor_dolar
+dolar=round(dolar,2)
+dolar=str(dolar)
+print("Tienes $"+dolar+" dólares")
